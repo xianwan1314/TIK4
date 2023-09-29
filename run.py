05df42861105f4d09e145dc2fe999a8363b2f1c2
ColdWindScholar <3590361911@qq.com>
2023-09-29 18:37:22 +0800

    first

--- run.py
+++ run.py
@@ -654,6 +654,23 @@ def packChoo(project):
         print("\033[33m [55] 循环打包 [66] 打包Super [77] 打包Payload [88]菜单\033[0m")
         print("  --------------------------------------")
         filed = input("  请输入对应序号：")
+        if filed == '0':
+            op_menu = input("  输出文件格式[1]br [2]dat [3]img:")
+            if op_menu == '1':
+                isbr = 1
+                isdat = 1
+            elif op_menu == '2':
+                isbr = 0
+                isdat = 1
+            else:
+                isbr = 0
+                isdat = 0
+            if settings.diyimgtype == '1':
+                syscheck = input("手动打包所有分区格式为：[1]ext4 [2]erofs")
+                if syscheck == '2':
+                    imgtype = "erofs"
+                else:
+                    imgtype = "ext"
 
 
 def unpack(file, info, project):
