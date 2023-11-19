cf7a84e8cd9579caed87c2a4c542b4146fa86863
ColdWindScholar <3590361911@qq.com>
2023-11-19 14:14:07 +0800

    Fix Some Bugs

--- build.py
+++ build.py
@@ -1,10 +1,40 @@
 import os
 import platform
 import shutil
+import zipfile
+
 import banner
+
 local = os.getcwd()
 print(f'\033[31m {banner.banner1} \033[0m')
 print(f'Build for {platform.system()}')
+if platform.system() == 'Linux':
+    name = 'TIK-linux.zip'
+else:
+    name = 'TIK-win.zip'
+
+
+def zip_folder(folder_path):
+    # 获取文件夹的绝对路径和文件夹名称
+    abs_folder_path = os.path.abspath(folder_path)
+
+    # 创建一个同名的zip文件
+    zip_file_path = os.path.join(local, name)
+    archive = zipfile.ZipFile(zip_file_path, "w", zipfile.ZIP_DEFLATED)
+
+    # 遍历文件夹中的所有文件和子文件夹
+    for root, dirs, files in os.walk(abs_folder_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            print(f"Adding: {file_path}")
+            # 将文件添加到zip文件中
+            archive.write(file_path, os.path.relpath(file_path, abs_folder_path))
+
+    # 关闭zip文件
+    archive.close()
+    print(f"Done!")
+
+
 os.system("pyinstaller -F run.py --exclude-module=numpy -i icon.ico")
 if os.name == 'nt':
     if os.path.exists(local + os.sep + "dist" + os.sep + "run.exe"):
@@ -40,3 +70,5 @@ if os.name == 'posix':
         for i in files:
             print(f"Chmod {os.path.join(root, i)}")
             os.system(f"chmod a+x {os.path.join(root, i)}")
+
+zip_folder(os.path.join(local))
